5347dfb90c386c8dcecb673cac91b6b6ec6c3e33
rohitrajt <rohitrajt@gmail.com>
2015-04-18 12:36:20 -0700

    Adding pig-script-runner module to unit test pig scripts

---
+++ pig-script-runner/src/test/groovy/org/rohit/pig/SamplePigSpec.groovy
@@ -0,0 +1,31 @@
+package org.rohit.pig
+
+import org.apache.pig.pigunit.PigTest
+import spock.lang.Specification
+
+
+class SamplePigSpec extends Specification {
+
+    void setup() {
+
+    }
+
+    void cleanup() {
+
+    }
+
+    def "execute" () {
+        given:
+            String[] args = ["n=2"]
+
+            String[] input = ["yahoo", "yahoo", "yahoo", "twitter", "facebook", "facebook", "linkedin"]
+
+            String[] output = ["(yahoo,3)", "(facebook,2)"]
+
+        when:
+            PigTest test = new PigTest("src/test/resources/test.pig", args)
+
+        then:
+            test.assertOutput("data", input, "queries_limit", output)
+    }
+}
